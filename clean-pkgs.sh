b87f29542b2b4af275148de9ac651a45597f08cf
cjstoddard <35096093+cjstoddard@users.noreply.github.com>
2024-02-08 20:30:01 -0600

    Update clean-pkgs.sh

--- clean-pkgs.sh
+++ clean-pkgs.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 diff -u pkg-baseline.txt package-chk.txt | grep -E "^\+" | sed -E 's/^\+//' | sed 's/install//g' | sed 's/-info//g' | sed '1d' > packages.txt
 sed -i 's/\s*$//g' packages.txt
 for i in $(cat packages.txt); do sudo apt purge $i -y; done

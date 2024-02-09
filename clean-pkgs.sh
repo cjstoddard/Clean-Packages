3b61f3ab3e2eece19a56bb908acebad708b6c0d8
cjstoddard <35096093+cjstoddard@users.noreply.github.com>
2024-02-08 20:18:00 -0600

    Create clean-pkgs.sh

---
+++ clean-pkgs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+diff -u pkg-baseline.txt package-chk.txt | grep -E "^\+" | sed -E 's/^\+//' | sed 's/install//g' | sed 's/-info//g' | sed '1d' > packages.txt
+sed -i 's/\s*$//g' packages.txt
+for i in $(cat packages.txt); do sudo apt purge $i -y; done
+sudo apt autopurge -y
+sudo apt clean
